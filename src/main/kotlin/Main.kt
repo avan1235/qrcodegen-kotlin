337bff3c145135583fb460356b8f6f1d022294a5
Maciej Procyk <maciej.procyk@jetbrains.com>
2025-10-16 18:17:03 +0200

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package `in`.procyk
+
+fun main() {
+
+}
---
+++ src/test/kotlin/QRCodeTest.kt
@@ -0,0 +1,10 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class QRCodeTest {
+
+    @Test
+    fun sampleTest() {
+        assertEquals(2 + 2, 4)
+    }
+}
