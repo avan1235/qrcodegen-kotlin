03d33d05a79e92c9f2178fea12ca818754755240
Maciej Procyk <maciej.procyk@jetbrains.com>
2025-10-18 19:14:25 +0200

    configure working publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
-group = "in.procyk"
-
 plugins {
     kotlin("multiplatform") version "2.2.20" apply false
     kotlin("plugin.serialization") version "2.2.20" apply false
-    id("io.github.adokky.quick-mpp") version "0.18" apply false
-    id("io.github.adokky.quick-publish") version "0.18" apply false
+    id("org.jetbrains.dokka") version "2.0.0" apply false
+    id("io.github.adokky.quick-mpp") version "0.19" apply false
+    id("com.vanniktech.maven.publish") version "0.34.0" apply false
 }
 
-subprojects {
+allprojects {
     group = "in.procyk"
+    version = "0.0.1"
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,9 +1,12 @@
-plugins {
-    id("io.github.adokky.quick-mpp")
-    id("io.github.adokky.quick-publish")
-}
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
 
-version = "0.0.1"
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.dokka")
+    id("io.github.adokky.quick-mpp")
+    id("com.vanniktech.maven.publish")
+}
 
 dependencies {
     commonMainImplementation("de.cketti.unicode:kotlin-codepoints:0.11.0")
@@ -11,16 +14,44 @@ dependencies {
 }
 
 mavenPublishing {
+    configure(
+        KotlinMultiplatform(
+            javadocJar = JavadocJar.Dokka("dokkaHtml"),
+            sourcesJar = true,
+        )
+    )
+
+    publishToMavenCentral()
+
+    signAllPublications()
+
     coordinates(artifactId = "qrcodegen")
+
     pom {
+        val githubRepoUrl = "github.com/avan1235/qrcodegen-kotlin"
         name = "qrcodegen"
         description = "QR code generator for Kotlin Multiplatform "
         inceptionYear = "2025"
+        url = "https://$githubRepoUrl"
         developers {
             developer {
                 id = "avan1235"
                 name = "Maciej Procyk"
+                email = "maciej@procyk.in"
                 url = "https://github.com/avan1235"
+                organizationUrl = "https://procyk.in"
+            }
+        }
+        scm {
+            url = "https://$githubRepoUrl"
+            connection = "scm:git:git://$githubRepoUrl.git"
+            developerConnection = "scm:git:git://$githubRepoUrl.git"
+        }
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
             }
         }
     }
